a21cdb15e84de4219336d6aff2075e02ebcee1f3
Zoviet <alexandr@asustem.ru>
2025-04-05 00:06:51 +0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# espocrm
-EspoCRM Lua SDK
+# EspoCRM Lua CRUD SDK
+
+EspoCRM is a single page application so the frontend uses REST API to connect with the backend. All operations you perform using the UI, you can implement via API calls using your programming language. You can learn how the API works if you trace what's going in the network tab in your browser console (press F12 key to open the console).
+
+[https://docs.espocrm.com/development/api/crud/]
+
---
+++ espocms/api.lua
@@ -0,0 +1,142 @@
+local json = require('cjson')
+local cURL = require("cURL")
+local date = require("date")
+local log = require('utils.log')
+local config = require('config.espocms')
+
+local _M = {}
+_M.result = nil
+_M.base = config.url..'/api/v1/'
+log.outfile = 'logs/espocms_'..os.date('%Y-%m-%d')..'.log' 
+log.level = 'trace'	
+
+function get_result(str,url)
+	local result, err = pcall(json.decode,str)
+	if result then
+		_M.result = json.decode(str)
+	else				
+		log.error(url..':'..err)
+		return nil,	err
+	end	
+	return _M.result
+end
+
+function get(url,data)
+	local str = ''
+	url =  _M.base..url..query(data)
+	local headers = {
+		'Content-type: application/json',
+		'Accept: application/json',
+		'X-Api-Key: '..config.key
+	}
+	local c = cURL.easy{		
+		url = url,	
+		httpheader  = headers,	
+		writefunction = function(st)	
+			str = str..st
+		end
+	}
+	local ok, err = c:perform()	
+	c:close()
+	if not ok then return nil, err end
+	local res,err = get_result(str,url)
+	if not res then return nil,err end
+	return res
+end
+
+function post(url,data)
+	local str = ''
+	url = _M.base..url
+	local headers = {
+		'Content-Type: application/json',
+		'Accept: application/json',
+		'X-Api-Key: '..config.key
+	}
+	local c = cURL.easy{		
+		url = url,
+		post = true,
+		postfields =  json.encode(data),  
+		httpheader  = headers,
+		writefunction = function(st)		
+			str = str..st
+		end
+	}
+	local ok, err = c:perform()	
+	c:close()
+	if not ok then return nil, err end
+	local res,err = get_result(str,url)
+	if not res then return nil,err end
+	return res
+end
+
+function put(url,data)
+	local str = ''
+	url = _M.base..url
+	local headers = {
+		'Content-Type: application/json',
+		'Accept: application/json',
+		'X-Api-Key: '..config.key
+	}
+	local c = cURL.easy{		
+		url = url,
+		put = true,
+		postfields =  json.encode(data),  
+		httpheader  = headers,
+		writefunction = function(st)		
+			str = str..st
+		end
+	}
+	local ok, err = c:perform()	
+	c:close()
+	if not ok then return nil, err end
+	local res,err = get_result(str,url)
+	if not res then return nil,err end
+	return res
+end
+
+
+function delete(url)
+	local str = ''
+	url = _M.base..url
+	local headers = {
+		'Content-Type: application/json',
+		'Accept: application/json',
+		'X-Api-Key: '..config.key
+	}
+	local c = cURL.easy{		
+		url = url,
+		httpheader  = headers,
+		customrequest = 'DELETE',
+		writefunction = function(st)		
+			str = str..st
+		end
+	}
+	local ok, err = c:perform()	
+	c:close()
+	if not ok then return nil, err end
+	local res,err = get_result(str,url)
+	if not res then return nil,err end
+	return res
+end
+
+function _M.list(typ,searchParams)
+	return get(typ,{['searchParams']=json.encode(searchParams)})
+end
+
+function _M.read(typ,id)
+	return get(typ..'/'..id)
+end
+
+function _M.create(typ,payload)
+	return post(typ,payload)
+end
+
+function _M.update(typ,id,payload)
+	return put(typ..'/'..id,payload)
+end
+
+function _M.delete(typ,id)
+	return delete(typ..'/'..id)
+end
+
+return _M
